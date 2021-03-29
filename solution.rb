586c1bd214f3c913e4f337348f7251e8bb39ca35
Zecharias Mengis <zmengis@gmail.com>
2021-03-29 09:33:31 -0500

    commit tree e7a51b69f696769df054e6fea2f4e04e9e64bc96

--- solution.rb
+++ solution.rb
@@ -1,7 +1,39 @@
+require 'pry'
+
 class Deck
 
+    attr_accessor :array
+
+    stack_of_cards = []
+
+    def initialize(array_arg)
+        @array = array_arg
+    end
+
+    def choose_card
+        @array.delete_at(rand(array.length))
+    end
 end
 
 class Card
+
+    attr_reader :rank, :suit
+
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
+    end
+
+    def make_deck
+        ranks = %w{A 2 3 4 5 6 7 8 9 10 J Q K}
+        suits = %w{Spades Hearts Diamonds Clubs}
+            suits.each do |suit|
+                ranks.size.times do |i|
+                    stack_of_cards << Card.new( ranks[i], suit, i+1 )
+                    binding.pry
+                end
+            end
+    end
+
 
 end
